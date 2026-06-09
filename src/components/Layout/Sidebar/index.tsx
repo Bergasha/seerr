a846696374ab0bc3b588f7720ba57213ebd96850
Bergasha <bergasha@hotmail.com>
2026-06-09 13:53:08 +1000

    Refactor Sidebar component logo link structure

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -191,13 +191,8 @@ const Sidebar = ({
                   >
                     <div className="flex flex-shrink-0 items-center px-2">
                       <span className="w-full px-4 text-xl text-gray-50">
-                        <Link href="/" className="block">
-                          <span className="block text-xs font-semibold tracking-widest text-purple-400" style={{ marginLeft: '38%' }}>
-                            Shayflix
-                          </span>
-                          <div className="relative h-24 w-64">
-                            <Image src="/logo_full.svg" alt="Logo" fill />
-                          </div>
+                        <Link href="/" className="relative block h-24 w-64">
+                          <Image src="/logo_full.svg" alt="Logo" fill />
                         </Link>
                       </span>
                     </div>
@@ -259,18 +254,13 @@ const Sidebar = ({
             <div className="flex flex-1 flex-col overflow-y-auto pb-4">
               <div className="flex flex-shrink-0 items-center">
                 <span className="w-full px-4 py-2 text-2xl text-gray-50">
-                  <Link href="/" className="block">
-                    <span className="block text-xs font-semibold tracking-widest text-purple-400" style={{ marginLeft: '38%' }}>
-                      Shayflix
-                    </span>
-                    <div className="relative h-24">
-                      <Image
-                        src="/logo_full.svg"
-                        alt="Logo"
-                        fill
-                        loading="eager"
-                      />
-                    </div>
+                  <Link href="/" className="relative block h-24">
+                    <Image
+                      src="/logo_full.svg"
+                      alt="Logo"
+                      fill
+                      loading="eager"
+                    />
                   </Link>
                 </span>
               </div>
